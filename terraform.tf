f526ae3d8e16ed2bbbbbdb974b86683cf22d1d54
goldentechnologyrec <119066198+goldentechnologyrec@users.noreply.github.com>
2023-11-17 12:46:17 +0100

    Redimensionnement frontend t2.micro >> t3.small

--- terraform.tf
+++ terraform.tf
@@ -162,7 +162,7 @@ variable "settings" {
     },
     "omega_frontend" = {
       count         = 1          // the number of EC2 instances
-      instance_type = "t2.micro" // the EC2 instance
+      instance_type = "t3.small" // the EC2 instance
     }
   }
 }
